0efc1b224ca8d1ba5f5a4d80f3d4ce8cf3c2af24
Max Schopf <max.schopf@mailfence.com>
2022-06-22 10:43:21 +0200

    Testcreation

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,6 +1,17 @@
 package com.app;
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("H");
+		
+
+		Ordner level1 = new Ordner("level1", 2);
+		Ordner level2 = new Ordner("level2", 3);
+		Ordner level3 = new Ordner("level3", 4);
+
+		level2.add(level3);
+		level1.add(level2);
+
+		level1.ausgabe("");
+
+
 	}
 }
